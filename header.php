ff7a5ed471245283f173ebc1491710f11fad2c81
Mikkay Gabinete <139051304+mikkz2@users.noreply.github.com>
2023-08-05 21:27:32 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/83c0f4a797.js" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link href="css/header.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <div class="nav">
+                <input type="checkbox" id="nav-check">
+                <div class="nav-header">
+                    <div class="nav-title">
+                        Go Explore
+                    </div>
+                </div>
+                <div class="nav-btn">
+                    <label for="nav-check">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </label>
+                </div>
+
+                <div class="nav-links">
+                    <a class="nav-link-1" href="#">Let's Explore</a>
+                    <a class="nav-link-2" href="#">Where to Go</a>
+                    <a class="nav-link-3" href="#">Calendar of Activities</a>
+                    <a class="nav-link-4" href="#">Love Our Planet</a>
+                    <a class="nav-link-5" href="#">Favorites</a>
+                    <a class="nav-search-icon" href="#">
+                        <i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i>
+                    </a>
+                    <a class="nav-user-icon" href="#">
+                        <i class="fa-solid fa-user" style="color: #ffffff;"></i>
+                    </a>
+                </div>
+            </div>
+        </header>
+    </body>
+</html>
---
+++ learn_more.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>LEARN MORE</title>
+    <link href="css/learn_more.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <section id="learnmore" class="container-fluid">
+        <div class="row justify-content-center" id="learnmore-container">
+            <!-- Dynamic content will be added here -->
+        </div>
+    </section>
+
+    <?php include 'footer.php'; ?>
+
+    <script src="js/learn_more.js"></script>
+</body>
+
+</html>
